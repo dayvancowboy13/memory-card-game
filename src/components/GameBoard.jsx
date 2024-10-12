76e1e021499857d4decfde669962f776ab0abfaa
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-10-12 09:12:28 -0700

    Add scoring functionality
    Keeps track of current score and records high score when player "loses"

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -26,35 +26,54 @@ export default function GameBoard() {
 
     const [deck, setDeck] = useState(shuffleDeck);
     const [clickedCards, setClickedCards] = useState(Array(characters.length).fill(false, 0))
-    const [count, setCount] = useState(0);
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
 
+    function checkClickedCard(id) {
+        if (!clickedCards[id]) {
+            setDeck(shuffleDeck);
+            setScore(() => score + 1);
+            setClickedCards(...clickedCards[id] = true);
+        } else {
+            log('already clicked that card; resetting')
+            score > highScore ?
+                setHighScore(score)
+                : null
+            setScore(0);
 
-    log(clickedCards)
+            // reset clicked
+            setClickedCards(Array(characters.length).fill(false, 0))
+        }
+    }
 
-    log(count);
-    log(deck);
+    // log(count);
+    // log(deck);
 
     return (
-        <div className='board'>
-            {deck.map((index) => {
-                // console.log(key.url);
-                return (
-                    <>
-                        <Card
-                            id={index}
-                            key={characters[index].name}
-                            imageURL={characters[index].url}
-                            cardTitle={characters[index].name}
-                            clicked={clickedCards[index]}
-                            onClick={(cardId) => {
-                                setDeck(shuffleDeck);
-                                setCount(() => count + 1);
-                                setClickedCards(...clickedCards[cardId] = true);
-                            }
-                            } />
-                    </>)
+        <>
+            <div className='scores'>
+                <h2>Current Score: {score}</h2>
+                <h2>High Score: {highScore}</h2>
+            </div>
+            <div className='board'>
+                {deck.map((index) => {
+                    // console.log(key.url);
+                    return (
+                        <>
+                            <Card
+                                id={index}
+                                key={characters[index].name}
+                                imageURL={characters[index].url}
+                                cardTitle={characters[index].name}
+                                clicked={clickedCards[index]}
+                                onClick={(cardId) => {
+                                    checkClickedCard(cardId);
 
-            })}
-        </div>
-    )
-} ``
+                                }
+                                } />
+                        </>)
+
+                })}
+            </div>
+        </>)
+}
